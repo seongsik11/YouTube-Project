24f42833c136bc4e9183c8e184271f01b953b505
배성진 <baeseongjin0311@gmail.com>
2023-03-28 11:12:41 +0900

    yes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 function App() {
-  return (
-    <div>
-        <h1 style={{color: "red", textAlign: "center"}}>
-          YouTube-Project
-        </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 style={{color: "red", textAlign: "center"}}>
+                YouTube-Project
+            </h1>
+            <footer style={{bottom : 0, position : "absolute", fontSize : "30px"}}>
+                youtube project kkk
+            </footer>
+        </div>
+
+    );
 }
 
 export default App;
+
