a6f701ca2e2598e8e5825c834f12f459ec5248c2
임성식 <imseongsig@imseongsigs-MacBook-Air.local>
2023-03-28 10:51:53 +0900

    title-commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from "react";
 function App() {
   return (
     <div>
-        <h1>
-          dfsdfsfsfsdfsjldkfjslkdfjslkdfjsaldkfjalskdjfsad
+        <h1 style={{color: "red", textAlign: "center"}}>
+          YouTube-Project
         </h1>
     </div>
   );
